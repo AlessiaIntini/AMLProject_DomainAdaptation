38fce3548923ff788c1a9c77345a77f7603c3434
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-24 16:44:46 +0100

    ,.

--- cityscapes.py
+++ cityscapes.py
@@ -57,7 +57,7 @@ class CityScapes(Dataset):
             image = self.transform(image)
             label = self.transform(label)
         #print("\n"+str(len(label)))
-        label = np.array(label).astype(np.int64)[np.newaxis, :]
+        #label = np.array(label).astype(np.int64)[np.newaxis, :]
         
         return  image,label
 
