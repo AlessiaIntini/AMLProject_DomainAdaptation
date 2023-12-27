18d79291d1db7d978cb0580ed1eed96ed5ee77c6
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-27 12:02:17 +0100

    d

--- cityscapes.py
+++ cityscapes.py
@@ -124,8 +124,8 @@ class CityScapes(Dataset):
         for fd in folders:
             fdpth = osp.join(gtpth, fd)
             lbnames = os.listdir(fdpth)
-            lbnames = [el for el in lbnames if 'labelTrainIds' in el]
-            names = [el.replace('_gtFine_labelTrainIds.png', '') for el in lbnames]
+            lbnames = [el for el in lbnames if 'color' in el]
+            names = [el.replace('_gtFine_color.png', '') for el in lbnames]
             lbpths = [osp.join(fdpth, el) for el in lbnames]
             gtnames.extend(names)
             self.labels.update(dict(zip(names, lbpths)))
