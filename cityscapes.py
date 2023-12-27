bbdeed78d48492b9e64f8ae4b95b1f28e078eff8
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-27 11:09:54 +0100

    b

--- cityscapes.py
+++ cityscapes.py
@@ -158,7 +158,8 @@ class CityScapes(Dataset):
         
         img = Image.open(impth).convert('RGB')
         label = Image.open(lbpth)
-        
+        img.show()
+        label.show()
         if self.mode == 'train' or self.mode == 'trainval':
             im_lb = dict(im = img, lb = label)
             im_lb = self.trans_train(im_lb)
