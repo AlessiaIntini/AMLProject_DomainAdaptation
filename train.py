fe8ffae053c7a5c0b1182aaa1e76c4e7e3a3d03f
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-31 13:19:42 +0100

    Resume from checkpoint added

--- train.py
+++ train.py
@@ -71,15 +71,15 @@ def val(args, model, dataloader, writer = None , epoch = None, step = None):
         return precision, miou
 
 
-def train(args, model, optimizer, dataloader_train, dataloader_val, comment=''):
+def train(args, model, optimizer, dataloader_train, dataloader_val,start_epoch, comment=''):
     #writer = SummaryWriter(comment=''.format(args.optimizer))
     writer = SummaryWriter(comment=comment)
     scaler = amp.GradScaler()
 
     loss_func = torch.nn.CrossEntropyLoss(ignore_index=255) #we should check if it's the right index to ignore, is it 255 or 19?
     max_miou = 0
-    step = 0
-    for epoch in range(args.num_epochs):
+    step = start_epoch
+    for epoch in range(start_epoch,args.num_epochs):
         lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
         model.train()
         tq = tqdm(total=len(dataloader_train) * args.batch_size)
@@ -98,13 +98,7 @@ def train(args, model, optimizer, dataloader_train, dataloader_val, comment=''):
                 loss3 = loss_func(out32, label.squeeze(1))
                 loss = loss1 + loss2 + loss3
 
-                #if i == image_number and epoch % 2 == 0: #saves the first image in the batch to tensorboard
-                #    print('epoch {}, iter {}, loss1: {}, loss2: {}, loss3: {}'.format(epoch, i, loss1, loss2, loss3))
-                #    colorized_predictions , colorized_labels = CityScapes.visualize_prediction(output, label)
-                #    writer.add_image('epoch%d/iter%d/predicted_labels' % (epoch, i), np.array(colorized_predictions), step, dataformats='HWC')
-                #    writer.add_image('epoch%d/iter%d/correct_labels' % (epoch, i), np.array(colorized_labels), step, dataformats='HWC')
-                #    writer.add_image('epoch%d/iter%d/original_data' % (epoch, i), np.array(data[0].cpu(),dtype='uint8'), step, dataformats='CHW')
-
+                
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
@@ -184,18 +178,7 @@ def parse_args():
                        type=int,
                        default=5,
                        help='How often to perform validation (epochs)')
-    ##parse.add_argument('--crop_height',
-    #                   type=int,
-    #                   default=512,
-    #                   help='Height of cropped/resized input image to modelwork')
-    #parse.add_argument('--crop_width',
-    #                   type=int,
-    #                   default=1024,
-    #                   help='Width of cropped/resized input image to modelwork')
-    # parse.add_argument('--crop_ratio',
-    #                    type=float,
-    #                    default=0.5,
-    #                    help='Ratio of cropped image (width and height) to input image')
+    
     parse.add_argument('--batch_size',
                        type=int,
                        default=4, #2
@@ -248,10 +231,10 @@ def parse_args():
                        type=str,
                        default='',
                        help='Optional comment to add to the model name and to the log.')
-    #parse.add_argument('--data_transformations',
-    #                   type=int,
-    #                   default=0,
-    #                   help='Select the data transformations to apply to the dataset. 0: no transformations, 1 : data augmentation')#1: random crop, 2: random crop and random horizontal flip, 3: random crop and random scale, 4: random crop, random horizontal flip and random scale.')
+    parse.add_argument('--resume',
+                       type=bool,
+                       default=False,
+                       help='Select if you want to resume from the last checkpoint')
     return parse.parse_args()
 
 
@@ -264,19 +247,9 @@ def main():
     
     cropsize = (512,1024)
 
-    transformations = ExtCompose([ExtResize(cropsize), ExtToTensor()]) #ExtRandomHorizontalFlip(),
+    transformations = ExtCompose([ExtResize(cropsize), ExtToTensor()])
 
-    #transformations = transforms.Compose([
-    #        transforms.Resize(cropsize),
-    #        transforms.ToTensor(),
-    #        ])
-    
-    #match args.data_transformations:
-    #    case 0:
-    #        transformations = ExtCompose([ExtResize((args.crop_height, args.crop_width)), ExtToTensor()]) #ExtRandomHorizontalFlip(),
-    #    case 1:
-    #        transformations = ExtCompose([ExtScale(random.choice([0.75,1,1.25,1.5,1.75,2]),interpolation=Image.Resampling.BILINEAR),ExtRandomCrop((args.crop_height, args.crop_width)), ExtToTensor()])
-    
+ 
     eval_transformations = ExtCompose([ExtScale(0.5,interpolation=Image.Resampling.BICUBIC), ExtToTensor()])
     
     
@@ -314,11 +287,32 @@ def main():
         print('not supported optimizer \n')
         return None
     
+    start_epoch = 0
+    
+
+    for check in os.listdir('./checkpoints'):
+        if 'latest_' in check:
+
+            start_epoch_tmp = int(check.split('_')[1].replace('.pth',''))
+
+            if start_epoch_tmp >= start_epoch:
+                start_epoch = start_epoch_tmp+1
+                pretrain_path = "checkpoints/"+check
+
+    #if args.resume and "latest_" in os.listdir("./checkpoints"):
+    #    model
+
+    if start_epoch > 0:
+        checkpoint = torch.load(pretrain_path)
+        model.module.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+
     
     match args.mode:
         case 'train':
             ## train loop
-            train(args, model, optimizer, dataloader_train, dataloader_val, comment="_{}_{}_{}_{}".format(args.mode,args.dataset,args.batch_size,args.learning_rate))
+            train(args, model, optimizer, dataloader_train, dataloader_val,start_epoch, comment="_{}_{}_{}_{}".format(args.mode,args.dataset,args.batch_size,args.learning_rate))
         case 'test':
             writer = SummaryWriter(comment="_{}_{}_{}_{}".format(args.mode,args.dataset,args.batch_size,args.learning_rate))
             val(args, model, dataloader_val,writer=writer,epoch=0,step=0)
