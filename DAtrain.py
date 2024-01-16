786569552f676bad47f4786607bfdcb4002776b1
peppe-sc <giuseppe.scarso@hotmail.it>
2024-01-16 23:56:32 +0100

    test da

--- DAtrain.py
+++ DAtrain.py
@@ -6,7 +6,7 @@ from cityscapes import CityScapes
 from GTA5 import GTA5
 import torchvision.transforms as transforms
 from torchvision.transforms import v2
-from utils import ExtCompose, ExtResize, ExtToTensor, ExtTransforms, ExtRandomHorizontalFlip , ExtScale , ExtRandomCrop
+from utils import ExtCompose, ExtResize, ExtToTensor, ExtTransforms, ExtRandomHorizontalFlip , ExtScale , ExtRandomCrop, ExtColorJitter
 import torch
 from torch.utils.data import DataLoader, Subset
 import logging
@@ -22,6 +22,9 @@ import os
 from PIL import Image
 from pathlib import Path
 from sklearn.model_selection import train_test_split
+import torch.nn.functional as F
+from torch.autograd import Variable
+
 
 logger = logging.getLogger()
 
@@ -154,46 +157,90 @@ def train(args, model, optimizer, dataloader_train, dataloader_val,start_epoch, 
     val(args, model, dataloader_val, writer, epoch, step)
 
 
-def train_and_adapt(args, model, optimizer,optimizer_D1,optimizer_D2,model_D1,model_D2, dataloader_train, dataloader_val,start_epoch, comment=''):
+def train_and_adapt(args, model, model_D1, optimizer,optimizer_D1, dataloader_source, dataloader_target, dataloader_val, start_epoch, comment=''):
     #writer = SummaryWriter(comment=''.format(args.optimizer))
     writer = SummaryWriter(comment=comment)
     scaler = amp.GradScaler()
 
     loss_func = torch.nn.CrossEntropyLoss(ignore_index=255) 
+    bce_loss = torch.nn.MSELoss()
     max_miou = 0
     step = start_epoch
+    source_label = 0
+    target_label = 1
     for epoch in range(start_epoch,args.num_epochs):
-
-        loss_seg_value1 = 0
-        loss_adv_target_value1 = 0
-        loss_D_value1 = 0
-
-        loss_seg_value2 = 0
-        loss_adv_target_value2 = 0
-        loss_D_value2 = 0
-
-
-
         lr = poly_lr_scheduler(optimizer, args.learning_rate, iter=epoch, max_iter=args.num_epochs)
+
+        discr_lr = poly_lr_scheduler(optimizer_D1,0.001,iter=epoch,max_iter=args.num_epochs)
+
         model.train()
-        tq = tqdm(total=len(dataloader_train) * args.batch_size)
-        tq.set_description('epoch %d, lr %f' % (epoch, lr))
+        model_D1.train()
+
+
+        tq = tqdm(total=len(tuple(zip(dataloader_source, dataloader_target))) * args.batch_size)
+        tq.set_description('epoch %d, lr %f, lr_discr %f' % (epoch, lr,discr_lr))
+
         loss_record = []
         #image_number = random.randint(0, len(dataloader_train) - 1)
-        for i, (data, label) in enumerate(dataloader_train):
-            data = data.cuda()
-            label = label.long().cuda()
+        for i, ((src_x, src_y), (trg_x, _)) in enumerate(zip(dataloader_source, dataloader_target)):
+            trg_x = trg_x.cuda()
+            scr_x = scr_x.cuda()
+            scr_y = scr_y.long().cuda()
+
             optimizer.zero_grad()
+            optimizer_D1.zero_grad()
+
+            #Train G
+
+            #Train with source
+            for param in model_D1.parameters():
+                param.requires_grad = False
+
+        
             with amp.autocast():
-                output, out16, out32 = model(data)
-                loss1 = loss_func(output, label.squeeze(1))
-                loss2 = loss_func(out16, label.squeeze(1))
-                loss3 = loss_func(out32, label.squeeze(1))
+                output_s, out16_s, out32_s = model(scr_x)
+                loss1 = loss_func(output_s, scr_y.squeeze(1))
+                loss2 = loss_func(out16_s, scr_y.squeeze(1))
+                loss3 = loss_func(out32_s, scr_y.squeeze(1))
                 loss = loss1 + loss2 + loss3
 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
+
+
+            #Train with target
+            with amp.autocast():
+                output_t, out16_t, out32_t = model(trg_x)
+
+                D_out1 = model_D1(F.softmax(out32_t))
+
+                loss_adv_target1 = bce_loss(D_out1,Variable(torch.FloatTensor(D_out1.data.size()).fill_(source_label)).cuda())
+
+                loss = 0.1*loss_adv_target1
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            #Train D
+            for param in model_D1.parameters():
+                param.requires_grad = True
+
+            D_out1_s = model_D1(F.softmax(out32_s))
+            loss_d1_s = bce_loss(D_out1_s,Variable(torch.FloatTensor(D_out1.data.size()).fill_(source_label)).cuda())
+            scaler.scale(loss_d1_s).backward()
+            
+            
+            D_out1_t = model_D1(F.softmax(out32_t))
+            loss_d1_t = bce_loss(D_out1_t,Variable(torch.FloatTensor(D_out1.data.size()).fill_(target_label)).cuda())    
+            scaler.scale(loss_d1_t).backward()
+            
+            
+            scaler.step(optimizer_D1)
+            scaler.step(optimizer)
+            scaler.update()
+
 
             tq.update(args.batch_size)
             tq.set_postfix(loss='%.6f' % loss)
@@ -201,6 +248,7 @@ def train_and_adapt(args, model, optimizer,optimizer_D1,optimizer_D2,model_D1,mo
             writer.add_scalar('loss_step', loss, step)
             loss_record.append(loss.item())
         tq.close()
+        
         loss_train_mean = np.mean(loss_record)
         writer.add_scalar('epoch/loss_epoch_train', float(loss_train_mean), epoch)
         print('loss for train : %f' % (loss_train_mean))
@@ -378,10 +426,26 @@ def main():
         target_dataset = CityScapes(root = "./Cityscapes/Cityspaces", split = 'train',transforms=transformations)
 
         cropsize = (720,1280)
-        transformations = ExtCompose([ExtRandomCrop(cropsize), ExtRandomHorizontalFlip(), ExtToTensor()])
+        transformations = ExtCompose([ExtRandomCrop(cropsize), ExtRandomHorizontalFlip(), ExtColorJitter(0.5,0.5,0.5,0.5), ExtToTensor()])
         source_dataset = GTA5(root = Path("/content"), transforms=transformations)
-
         
+        transformations = ExtCompose([ExtToTensor()])
+        val_dataset = CityScapes(root= "./Cityscapes/Cityspaces", split='val',transforms=transformations)#eval_transformations)
+
+        dataloader_source = DataLoader(source_dataset,
+                    batch_size=args.batch_size,
+                    shuffle=False,
+                    num_workers=args.num_workers,
+                    pin_memory=False,
+                    drop_last=True)
+
+
+        dataloader_target = DataLoader(target_dataset,
+                    batch_size=args.batch_size,
+                    shuffle=False,
+                    num_workers=args.num_workers,
+                    pin_memory=False,
+                    drop_last=True)
 
         model_D1.train()
         model_D1.cuda()
@@ -389,7 +453,7 @@ def main():
         
 
         optimizer_D1 = torch.optim.Adam(model_D1.parameters(), lr=args.learning_rate_D, betas=(0.9, 0.99))
-        optimizer_D1.zero_grad()
+        #optimizer_D1.zero_grad()
 
 
         bce_loss = torch.nn.BCEWithLogitsLoss()
@@ -465,6 +529,8 @@ def main():
         case 'test':
             writer = SummaryWriter(comment="_{}_{}_{}_{}".format(args.mode,args.dataset,args.batch_size,args.learning_rate))
             val(args, model, dataloader_val, writer=writer,epoch=0,step=0)
+        case 'adapt':
+            train_and_adapt(args, model,model_D1, optimizer,optimizer_D1, dataloader_source,dataloader_target, dataloader_val,start_epoch, comment="_{}_{}_{}_{}".format(args.mode,args.dataset,args.batch_size,args.learning_rate))
         case _:
             print('not supported mode \n')
             return None
