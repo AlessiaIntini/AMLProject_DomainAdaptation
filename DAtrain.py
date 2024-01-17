85a7e6cdf1e69e04d0251c265c5104b3f5b5d087
peppe-sc <giuseppe.scarso@hotmail.it>
2024-01-17 11:55:23 +0100

    .

--- DAtrain.py
+++ DAtrain.py
@@ -229,12 +229,12 @@ def train_and_adapt(args, model, model_D1, optimizer,optimizer_D1, dataloader_so
                 param.requires_grad = True
 
             D_out1_s = model_D1(F.softmax(out32_s).float())
-            loss_d1_s = bce_loss(D_out1_s,Variable(torch.FloatTensor(D_out1.data.size()).fill_(source_label)).cuda())
+            loss_d1_s = bce_loss(D_out1_s,Variable(torch.FloatTensor(D_out1_s.data.size()).fill_(source_label)).cuda())
             scaler.scale(loss_d1_s).backward()
             
             
             D_out1_t = model_D1(F.softmax(out32_t).float())
-            loss_d1_t = bce_loss(D_out1_t,Variable(torch.FloatTensor(D_out1.data.size()).fill_(target_label)).cuda())    
+            loss_d1_t = bce_loss(D_out1_t,Variable(torch.FloatTensor(D_out1_t.data.size()).fill_(target_label)).cuda())    
             scaler.scale(loss_d1_t).backward()
             
             
